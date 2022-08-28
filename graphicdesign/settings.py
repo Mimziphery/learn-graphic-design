0d30b52e75181dae9b291c260a68a2d201507d55
Mimoza Mickoska <63194172+Mimziphery@users.noreply.github.com>
2022-08-28 22:06:10 +0200

    static root updated

--- graphicdesign/settings.py
+++ graphicdesign/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,4 +126,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = "/main/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
