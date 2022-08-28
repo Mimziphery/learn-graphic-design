8024380050155f508ac7f88349e47cb3dff55728
Mimoza Mickoska <63194172+Mimziphery@users.noreply.github.com>
2022-08-28 22:18:52 +0200

    updated models

--- main/models.py
+++ main/models.py
@@ -1,7 +1,3 @@
-from cProfile import label
-from codecs import charmap_build
-from pickle import TRUE
-from turtle import ondrag
 from django.db import models
 from django.contrib.auth.models import User
 
