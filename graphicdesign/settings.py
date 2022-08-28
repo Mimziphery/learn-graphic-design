4d67f7ffc5bc5a6abc435194d13e78f513ddc73e
Mimoza Mickoska <63194172+Mimziphery@users.noreply.github.com>
2022-08-28 23:10:47 +0200

    message

--- graphicdesign/settings.py
+++ graphicdesign/settings.py
@@ -126,6 +126,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_DIRS=[(os.path.join(BASE_DIR,'static'))]
